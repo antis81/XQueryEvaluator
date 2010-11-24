3fade07dc5bd8dd6a59c45a97d7512028142152d
Nils Fenner <nilsfenner@nilsmac.fritz.box>
2010-11-24 22:47:34 +0100

    using the stylesheet resource

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <QtGui/QApplication>
 #include "XQEMainWindow.h"
 
+#include <QtCore/QFile>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile f(":/DarkBlue.css");
+    if (f.open(QIODevice::ReadOnly))
+        a.setStyleSheet(f.readAll());
+
     XQEMainWindow w;
     w.show();
     return a.exec();
