fb6d435b55c6f214a124e8a844308c0f4c6e9248
Nils Fenner <nilsfenner@web.de>
2011-04-18 20:35:56 +0200

    added missing license comments and cleanup

--- libs/ui/DockWidgets.cpp
+++ libs/ui/DockWidgets.cpp
@@ -1,3 +1,22 @@
+/*
+**    Copyright (c) 2011 by Nils Fenner
+**
+**    This file is part of XQueryEvaluator.
+**
+**    XQueryEvaluator is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**    XQueryEvaluator is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with XQueryEvaluator.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "DockWidgets.h"
 
 #include <QtGui/QMainWindow>
@@ -39,19 +58,22 @@ The widget can be set by another instance before calling show.
 */
 void DockWidgets::registerDockWidget( const QString &key, Qt::DockWidgetArea area )
 {
-    registerDockWidget(key, QString(), area );
+        AssignedDockWidget * dw = _assignedDockWidgets.insert(key, new AssignedDockWidget()).value();
+
+        dw->setArea(area);
+        dw->setContentWidget(0);
 }
 
-/**
-Registers a dock widget for a fixed area.
-*/
-void DockWidgets::registerDockWidget( const QString &key, const QString &widgetClassName, Qt::DockWidgetArea area )
-{
-    AssignedDockWidget * dw = _assignedDockWidgets.insert(key, new AssignedDockWidget()).value();
+///**
+//Registers a dock widget for a fixed area.
+//*/
+//void DockWidgets::registerDockWidget( const QString &key, const QString &widgetClassName, Qt::DockWidgetArea area )
+//{
+//    AssignedDockWidget * dw = _assignedDockWidgets.insert(key, new AssignedDockWidget()).value();
 
-    dw->setArea(area);
-    dw->setContentWidget(0);
-}
+//    dw->setArea(area);
+//    dw->setContentWidget(0);
+//}
 
 /**
 Show the assigned dock widget for key.
--- libs/ui/DockWidgets.h
+++ libs/ui/DockWidgets.h
@@ -1,3 +1,22 @@
+/*
+**    Copyright (c) 2011 by Nils Fenner
+**
+**    This file is part of XQueryEvaluator.
+**
+**    XQueryEvaluator is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**    XQueryEvaluator is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with XQueryEvaluator.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef DOCKWIDGETS_H
 #define DOCKWIDGETS_H
 
@@ -23,7 +42,7 @@ public:
     void setOwnerWindow(QMainWindow * window);
 
     void registerDockWidget( const QString &key, Qt::DockWidgetArea area );
-    void registerDockWidget( const QString &key, const QString &widgetClassName, Qt::DockWidgetArea area );
+    //void registerDockWidget( const QString &key, const QString &widgetClassName, Qt::DockWidgetArea area );
 
     bool show(const QString &key, QWidget * content = 0);
     void hide(const QString &key);
